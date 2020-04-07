2e6392696dcab8394cd2e0e1c1a046b55869c90c
Daniel McDonald <dajmcdon@gmail.com>
2020-04-06 19:58:34 -0700

    code for plots and group norms

--- extras/lasso_sims.R
+++ extras/lasso_sims.R
@@ -14,67 +14,26 @@ eps <- rnorm(n, mean = 0, sd=1)
 
 beta_star <- c(rep(5,5), c(5,-5,2,0,0), rep(-5,5), c(2,-3,8,0,0), rep(0,(p-20)))
 
-
-# we generate the data so that there are groups of 5 vars each
-grp <- rep(1:20, each=5)
-
 y <- X%*%beta_star + eps
 
-# Now we need to try the lassos
-out <- gglasso(X,y,group=grp, loss='ls')
-
-beta50 <- out$beta[,50]
-
-# Next we try ls_sparse
-out_sp <- gglasso(X,y,group=grp, loss = 'ls_sparse')
-
-beta50_sparse <- out$beta[,50]
-
-
-###############################################################################
-## A much smaller data set for debugging
-
-
-# Set the n and p values
-set.seed(1010)
-n <- 100
-p <- 9
-
-# There are different ways to generate a data matrix, I'm gonna be a dummy
-
-X <- matrix(data = rnorm(n*p, mean=0, sd=1), nrow = n, ncol = p)
-
-eps <- rnorm(n, mean = 0, sd=1)
-
-beta_star <- c(rep(5,3), c(-5,0,-3),rep(0,3))
-
-
-# we generate the data so that there are groups of 5 vars each
-grp <- rep(1:3, each=3)
-
-y <- X%*%beta_star + eps
+groups <- rep(1:20, each=5)
 
 # Now we need to try the lassos
-out <- gglasso(X,y,group=grp, loss='ls')
 
-#beta50 <- out$beta[,50]
+out <- gglasso(X, y, group = groups, loss = 'ls')
 
-# Next we try ls_sparse
-out_sp <- gglasso(X,y,group=grp, loss = 'ls_sparse')
-
-#beta50_sparse <- out$beta[,50]
-
-group_norm <- function(x, grp) sum(by(x, grp, function(x) sqrt(sum(x^2))))
-sp_group_norm <- function(x, grp, alp=.05) group_norm(x,grp)*(1-alp) + alp*sum(abs(x))
+out_sp <- gglasso(X,y, group = groups, loss="ls_sparse")
 
 
+grp_norm <- function(x, grp, pf = sqrt(table(grp))){
+  out = by(x,grp,function(x) sqrt(sum(x^2)))
+  sum(out*pf)
+}
 
+sparse_grp_norm <- function(x, grp, alpha = .05, pf = sqrt(table(grp))){
+  out1 = grp_norm(x,grp,pf)
+  (1-alpha)*out1 + alpha*sum(abs(x))
+}
 
-# Dan's code
-par(mfrow=c(2,2))
-plot(out)
-plot(out_sp)
-b1 = apply(out$beta, 2, sd)
-b2 = apply(out_sp$beta, 2, sd)
-matplot(b1, t(out$beta), ty='l', lty=1, col = grp)
-matplot(b2, t(out_sp$beta), ty='l', lty=1, col = grp)
+matplot(apply(out$beta, 2, grp_norm, grp=groups), t(out$beta), ty='l',lty=1,col=2)
+matlines(apply(out_sp$beta, 2, sparse_grp_norm, grp=groups), t(out_sp$beta), lty=1,col=4)
